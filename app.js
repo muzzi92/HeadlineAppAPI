11d73695bf69c06c6ab4b1714b50f2e307dd0ce2
Muzzi Aldean <amiraldean@gmail.com>
2018-07-21 18:42:52 +0100

    Rendered view on index route

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require("express");
 var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+var apiKey = "2172ba9d-ee18-4320-836f-ac58eb646078";
+var Guardian = require('guardian-js');
+var api = new Guardian(apiKey, false);
+
 
 app.get("/", function(req, res){
-  res.render("index");
-  });
+  res.render('index');
+});
 
 app.listen(3000, function(){
   console.log("Serve's Up Dude!");
